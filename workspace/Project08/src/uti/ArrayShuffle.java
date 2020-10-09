3079e03c72aadd962f814eb1c851c26516eda509
Rositsa Zaharieva <71489061+rebrosi@users.noreply.github.com>
2020-10-09 18:17:29 +0300

    09.10

--- workspace/Project08/src/uti/ArrayShuffle.java
+++ workspace/Project08/src/uti/ArrayShuffle.java
@@ -13,7 +13,13 @@ public class ArrayShuffle {
 		
 		for(int i = 0; i < 10; i++){
 			System.out.print("Enter a number ");
-			randomNumberArray[rand.nextInt(10)] = sc.nextInt();
+			randomNumberArray[i] = sc.nextInt();
+		}
+		for (int i = 0; i < randomNumberArray.length; i++) {
+			int randomIndexToSwap = rand.nextInt(randomNumberArray.length);
+			int temp = randomNumberArray[randomIndexToSwap];
+			randomNumberArray[randomIndexToSwap] = randomNumberArray[i];
+			randomNumberArray[i] = temp;
 		}
 		for(int i = 0; i < 10; i++){
 			System.out.println(randomNumberArray[i]);
